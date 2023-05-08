fe18c92e15008719fd90425527a852127d5b1c62
VictoriaAlekseeva <vicalekseeva88@gmail.com>
2023-05-08 23:55:34 +0200

    feat: add solution for 5th task

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+const {readdir, readFile, appendFile, rm} = require('fs/promises')
+
+const soursePath = path.join(__dirname, 'styles' );
+const bundleFolder = path.join(__dirname, 'project-dist')
+const bundleFile = path.join(bundleFolder, 'bundle.css');
+
+async function createBundle() {
+  const bundleFiles = await readdir(bundleFolder, {withFileTypes: true});
+
+  for (const file of bundleFiles) {
+    if (file.name === 'bundle.css') {
+      await rm(bundleFile, { recursive: true })
+    }
+  }
+
+  const files = await readdir(soursePath, {withFileTypes: true});
+
+  const cssFiles = files.filter(file => file.isFile() && path.extname(file.name) === '.css');
+
+  for (const file of cssFiles) {
+    const css = await readFile(path.join(soursePath, file.name), 'utf-8');
+    appendFile(bundleFile, css, (err) => {
+      if (err) console.log(message.err)
+    })
+
+  }
+}
+
+createBundle()
